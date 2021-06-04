0e01f151f3fed8b8979cbc242f9949e2a121060c
mayongjie <516225899@qq.com>
2021-06-05 01:54:05 +0800

    fix：修复图表不能自适应窗口变化问题

--- src/pages/mytest.vue
+++ src/pages/mytest.vue
@@ -2,39 +2,35 @@
  * @Autor: eobeans
  * @Date: 2021-06-01 23:12:26
  * @LastEditors: eobeans
- * @LastEditTime: 2021-06-03 23:17:41
+ * @LastEditTime: 2021-06-05 01:44:59
  * @Version: 0.1.0
  * @Description: 
 -->
 <template>
 	<div class="layout">
-		<Layout>
-			<Layout :style="{padding: '0 12px 12px'}">
-					<Content :style="{padding: '12px', minHeight: '280px', background: '#fff' , margin:'24px'}">
-							<div style="height:400px">
-									<!-- <v-chart id="myPieChart" :options="pie"/> -->
-									<dev-line-chart
-										ref="zzqxtChart"
-										title="非居民税收所得类型占比情况"
-										:is-finished.sync="zzqxtFinished"
-										:height="400"
-										:data="barLineChartData"
-										:chartOpt="{}"
-										showLegend
-									>
-									</dev-line-chart>
-							</div>
-						</Content>
-			</Layout>
+		<Layout :style="{padding: '0 12px 12px'}">
+			<Content :style="{padding: '12px', minHeight: '280px', background: '#fff' , margin:'24px'}">
+				<div style="height:400px">
+						<dev-line-chart
+							ref="zzqxtChart"
+							title="非居民税收所得类型占比情况"
+							:is-finished.sync="zzqxtFinished"
+							:height="400"
+							:data="barLineChartData"
+							:chartOpt="{}"
+							showLegend
+						>
+						</dev-line-chart>
+				</div>
+			</Content>
 		</Layout>
 	</div>
 </template>
 <script>
-import ECharts from 'vue-echarts'
 import DevLineChart from '@/components/DevLineChart'
 export default {
+	name: 'testDevLineChart',
 	components: {
-		'v-chart': ECharts,
 		DevLineChart: DevLineChart
 	},
 	data(){
