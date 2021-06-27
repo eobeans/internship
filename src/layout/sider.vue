0f75a653f787b49713454d6887b4083464720429
mayongjie <516225899@qq.com>
2021-06-27 23:47:46 +0800

    alter：使用render替代模板

--- src/layout/sider.vue
+++ src/layout/sider.vue
@@ -2,83 +2,78 @@
  * @Autor: eobeans
  * @Date: 2021-06-03 13:29:27
  * @LastEditors: eobeans
- * @LastEditTime: 2021-06-20 19:06:52
+ * @LastEditTime: 2021-06-27 23:46:38
  * @Version: 0.1.0
  * @Description: 
 -->
-<template>
-  <div>
-		{{createMySider()}}
-  </div>
-</template>
 <script>
 // import setting from '../setting'
 import routes from '../router/label.js'
 const cloneDeep = require('lodash/cloneDeep')
+import { Sider, Submenu, MenuItem, Menu } from 'view-design';
 
 export default {
 	name: 'MySider',
 	inheritAttrs: false,
 	
+	components: {
+		'Sider': Sider,
+		'Submenu': Submenu,
+		'MenuItem': MenuItem,
+		'Menu': Menu
+	},
 	data(){
 		return{    
 			minHeight:0,
 			activeName: '1-1'
 		}
 	},
-	mounted(){
-		this.activeName = this.$router.currentRoute.name
-	},
-	methods:{
-		createMySider() {
-			console.log('sider render')
-			const _this = this
+	render(h) {
+		const _this = this
+		const myRoutes = cloneDeep(routes)
+		const createMenus = function () {
 			return (
-				<i-sider ref="side" hide-trigger collapsible collapsed-width="78" style="{background: '#fff'}">
-					<i-menu active-name={_this.activeName} theme="light" width="auto" style="{ minHeight: minHeight + 'px'}">
-						{_this.createMenus()}
-					</i-menu>
-				</i-sider>
-			)
-		},
-		createMenus() {
-			console.log('createMenus', routes)
-			const myRoutes = cloneDeep(routes)
-			const _this = this
-			
-			return (
-				myRoutes.map((route) => {
+				myRoutes.map(function(route) {
 					if (route.name === 'login' || route.name === 'register'){
 						console.log('login or register')
 					} else {
+						console.log('Submenu')
 						return (
-							<i-sub-menu name={route.name}>
+							<Submenu name={route.name}>
 								<template slot="title">
-										<icon type={route.icon} i-/>
+										<icon type={route.icon} />
 										{route.label}
 								</template>
-								{route.children&&route.children.length > 0 ? _this.createMenuItem(route.children) : null}
-							</i-sub-menu>
+								{route.children&&route.children.length > 0 ? createMenuItem(route.children) : null}
+							</Submenu>
 						)
 					}
 				})
 			)
-		},
-		createMenuItem(routeChild) {
-			if(!routeChild) {
-				return
-			}
-			const _this = this
-			console.log('createMenuItem')
+		}
+		const createMenuItem = function(routeChild) {
+			console.log('MenuItem')
 			return (
-				routeChild.map((item) => {
-					<i-menu-item name={item.name} on-click_native={_this.selectNav(item.name)}>
+				routeChild.map(function(item) {
+					<MenuItem name={item.name} on-click_native={_this.selectNav(item.name)}>
 						<icon type={item.icon} />
 						<span>{item.label}</span>
-					</i-menu-item>
+					</MenuItem>
 				})
 			)
-		},
+		}
+		return (
+			<Sider ref="side" hide-trigger collapsible collapsed-width="78" style="{background: '#fff'}">
+				<Menu active-name={_this.activeName} theme="light" width="auto" style="{ minHeight: minHeight + 'px'}">
+					{createMenus}
+				</Menu>
+			</Sider>
+		)
+	},
+	mounted(){
+		this.activeName = this.$router.currentRoute.name
+	},
+	methods:{
 		selectNav(val){
 			this.$router.push('/'+ val)
 		}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Autor: eobeans
  * @Date: 2021-05-27 22:42:25
  * @LastEditors: eobeans
- * @LastEditTime: 2021-06-20 17:12:11
+ * @LastEditTime: 2021-06-27 23:38:35
  * @Version: 0.1.0
  * @Description: 
  */
@@ -13,7 +13,6 @@ import VueResource from 'vue-resource'
 import store from './store/index'
 import './styles/index.scss'
 import ElementUI from 'element-ui'
-import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 import './mock/index'
 
@@ -21,15 +20,6 @@ Vue.use(VueResource)
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
-Vue.use(ViewUI, {
-  transfer: true,
-  size: 'large',
-  capture: false,
-  select: {
-      arrow: 'md-arrow-dropdown',
-      arrowSize: 20
-  }
-});
 
 new Vue({
   el: '#app',
