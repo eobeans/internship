d0489ee886fcde5cb83d854ca986f4a73d2e22bf
mayongjie <516225899@qq.com>
2021-08-08 15:23:52 +0800

    feat:添加截图下载功能

--- src/pages/collect/zoumadeng.vue
+++ src/pages/collect/zoumadeng.vue
@@ -2,12 +2,12 @@
  * @Autor: eobeans
  * @Date: 2021-07-31 14:18:34
  * @LastEditors: eobeans
- * @LastEditTime: 2021-07-31 14:34:40
+ * @LastEditTime: 2021-08-08 15:22:55
  * @Version: 0.1.0
  * @Description: 
 -->
 <template>
-	<div class="layout">
+	<div class="layout" id="wyyzmd">
     <div>
       <span> ElementUI走马灯 </span>
       <div>
@@ -29,9 +29,12 @@
         </div>
       </div>
     </div>
+    <el-button @click="downLoadPdf">下载PDF</el-button>
 	</div>
 </template>
 <script>
+import html2canvas from 'html2canvas'
+
 export default {
 	name: 'wyyzmd',
 	components: {
@@ -42,7 +45,50 @@ export default {
 	},
 	mounted(){},
 	methods:{
-	
+    downLoadPdf() {
+      // 生成快照
+      const convertToImage = (container, options = {}) => {
+        // 设置放大倍数
+        const scale = window.devicePixelRatio;
+
+        // 传入节点原始宽高
+        const width = container.offsetWidth;
+        const height = container.offsetHeight;
+
+        // html2canvas配置项
+        const ops = {
+          scale,
+          width,
+          height,
+          useCORS: true,
+          allowTaint: false,
+          ...options
+        };
+        
+        return html2canvas(container, ops).then(canvas => {
+          // 返回图片的二进制数据
+          const cancasImg = canvas.toDataURL("image/png");
+          this.downloadClick('test', cancasImg);
+        });
+      }
+
+      // 调用函数，取到截图的二进制数据，对图片进行处理（保存本地、展示等）
+      const element = document.getElementById('wyyzmd');
+      convertToImage(element);
+    },
+    downloadClick(fileName, canvasImg) {
+      const a = document.createElement('a')
+      //指定下载文件名称
+      a.href = canvasImg;
+      a.download = fileName
+      //a 标签 需要点击触发。所以强制给他分派一个点击事件
+      //创建一个鼠标事件
+      let event = document.createEvent("MouseEvents")
+      // 初始化鼠标事件
+      event.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+      // 指定元素对象触发事件
+      a.dispatchEvent(event)
+    }
 	}
 }
 </script>
