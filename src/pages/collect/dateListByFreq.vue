9e391ec969c0b72207c52c0c0d2324594554d2d4
mayongjie <516225899@qq.com>
2021-08-15 02:17:11 +0800

    feat:添加生成指定日期区间内各频率的时间数组（限制30条）

--- src/pages/collect/dateListByFreq.vue
+++ src/pages/collect/dateListByFreq.vue
@@ -2,7 +2,7 @@
  * @Autor: eobeans
  * @Date: 2021-08-14 11:05:04
  * @LastEditors: eobeans
- * @LastEditTime: 2021-08-14 12:15:42
+ * @LastEditTime: 2021-08-15 02:15:41
  * @Version: 0.1.0
  * @Description: 根据起止时间和事件频率生成日期列表
 -->
@@ -36,6 +36,10 @@ import _ from 'lodash'
 import dayjs from 'dayjs'
 const duration = require('dayjs/plugin/duration')
 dayjs.extend(duration)
+const quarterOfYear = require('dayjs/plugin/quarterOfYear')
+dayjs.extend(quarterOfYear)
+const isSameOrBefore = require('dayjs/plugin/isSameOrBefore')
+dayjs.extend(isSameOrBefore)
 const today = dayjs().format('YYYYMMDD')
 const startOfYear = dayjs().startOf('year').format('YYYYMMDD')
 export default {
@@ -65,8 +69,8 @@ export default {
         },
       ],
       startDate: startOfYear,
-      endDate: today,
-      selectFreq: 'M'
+      endDate: '20250930',
+      selectFreq: 'S'
     }
   },
   methods: {
@@ -76,7 +80,8 @@ export default {
       if (this.selectFreq === 'D') {
         const durationFreqD = dayjs.duration(dayjs(this.endDate).diff(dayjs(this.startDate))).asDays()
         console.log(durationFreqD)
-        for (let i = 0; i <= durationFreqD; i++) {
+        const forLength = durationFreqD > 29 ? 29 : durationFreqD
+        for (let i = 0; i <= forLength; i++) {
           rptDatList.push(dayjs(this.startDate).add(i, 'd').format('YYYYMMDD'))
         }
       } else if (this.selectFreq === 'M') {
@@ -88,15 +93,47 @@ export default {
         if (startFreqMDate === this.endDate) {
           rptDatList.push(startFreqMDate)
         } else {
-          for(let i = 0; i < durationFreqM; i++) {
+          const forLength = durationFreqM > 30 ? 30 : durationFreqM
+          for(let i = 0; i < forLength; i++) {
             if (dayjs(startFreqMDate).isBefore(this.endDate)) {
               rptDatList.push(dayjs(startFreqMDate).add(i, 'M').format('YYYYMMDD'))
             }
           }
         }
       } else if (this.selectFreq === 'Q') {
-        const durationY = _.subtract(_.toNumber(this.endDate.substring(0, 4)), _.toNumber(this.startDate.substring(0, 4)))
-        const durationM = _.subtract(_.toNumber(this.endDate.substring(4, 6)), _.toNumber(this.startDate.substring(4, 6)))
+        const startFreqQDate = dayjs(this.startDate).endOf('quarter')
+        for(let i = 0; i < 29; i++) {
+          const rptDat = dayjs(startFreqQDate).add(i, 'quarter').format('YYYYMMDD')
+          rptDatList.push(rptDat)
+          if (dayjs(rptDat).isAfter(this.endDate, 'day')) {
+            break
+          }
+        }
+        rptDatList.pop()
+      } else if (this.selectFreq === 'S') {
+        let startDateS = '0630'
+        if (dayjs(this.startDate).month() >= 6 && dayjs(this.startDate).month() < 12) {
+          startDateS = '1231'
+        }
+        const startFreqSDate = this.startDate.substring(0, 4) + startDateS
+        for(let i = 0; i < 29; i++) {
+          const rptDat = dayjs(startFreqSDate).add(_.multiply(i, 2), 'quarter').endOf('month').format('YYYYMMDD') // dayjs add 6个月 || 2个季度，不会自动从月末开始
+          rptDatList.push(rptDat)
+          if (dayjs(rptDat).isAfter(this.endDate, 'day')) {
+            break
+          }
+        }
+        rptDatList.pop()
+      } else if (this.selectFreq === 'Y') {
+        const startFreqYDate = dayjs(this.startDate).endOf('year')
+        for(let i = 0; i < 29; i++) {
+          const rptDat = dayjs(startFreqYDate).add(i, 'year').format('YYYYMMDD')
+          rptDatList.push(rptDat)
+          if (dayjs(rptDat).isAfter(this.endDate, 'day')) {
+            break
+          }
+        }
+        rptDatList.pop()
       }
       console.log('rptDatList', rptDatList)
     }
